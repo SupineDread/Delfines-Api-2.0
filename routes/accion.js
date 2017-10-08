0dbe4977ec03aeab0434f776351d22dbb113e07a
SupineDread <hazielfe@gmail.com>
2017-10-08 18:06:55 -0500

    Cosa

--- routes/accion.js
+++ routes/accion.js
@@ -4,7 +4,6 @@ const express = require('express');
 const api = express.Router();
 const accionController = require('../controllers/accion');
 
-//Aqui van las rutas
 //Todas las rutas deben recibir un id para que se guarde en el campo de remesa (ver modelo accion)
 
 module.exports = api;
