5d8d47ac340dd0fdfcddaa3949399aaa7e78dc58
SupineDread <hazielfe@gmail.com>
2017-10-23 19:22:01 -0500

    JWT inicio

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports={
   port: process.env.PORT || 8080,
-  //db: process.env.MONGODB || 'mongodb://admin:cacr2205@ds036577.mlab.com:36577/congelados-delfines',
-  db: process.env.MONGODB || 'mongodb://localhost:27017/delfines',
+  db: process.env.MONGODB || 'mongodb://admin:cacr2205@ds036577.mlab.com:36577/congelados-delfines',
   SECRET_TOKEN: 'miclavedetokensasibiensecretaybienproquenadiesabe',
 }
