4b07bd0669b9d0a3ef024fe75d882cc94e8f3a67
SupineDread <hazielfe@gmail.com>
2017-12-09 09:24:11 -0600

    Commit de pruebas

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
+const SHA256 = require('crypto-js/sha256');
+const gravatar = require('gravatar');
+
 module.exports={
   port: process.env.PORT || 8080,
   //db: process.env.MONGODB || 'mongodb://admin:cacr2205@ds036577.mlab.com:36577/congelados-delfines',
   db: process.env.MONGODB || 'mongodb://localhost:27017/delfines',
   SECRET_TOKEN: 'miclavedetokensasibiensecretaybienproquenadiesabe',
-  password: 'cacr2205',
-  email: 'cacr@mail.com',
-  pin: 10000,
-  role: 'ADMIN'
+  userAdmin: {
+    name: 'Administrador',
+    username: 'Administrador',
+    password: SHA256('cacr2205').toString(),
+    avatar: gravatar.url('cacr@mail.com', {s: '100', r: 'x', d: 'retro'}, true),
+    email: 'cacr@mail.com',
+    pin: 10000,
+    role: 'ADMIN'}
 }
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const SHA256 = require('crypto-js/sha256');
-const gravatar = require('gravatar');
 const user = require('./models/user');
 const app = require('./app');
 const config = require('./config');
@@ -11,17 +9,10 @@ mongoose.connect(config.db, {useMongoClient: true}, (err, res) =>{
         throw err;
     }else{
         console.log('Conexion correcta a la base de datos');
+        user.findOrCreate(config.userAdmin, (err, result) => {
+          console.log('Usuario administrador encontrado o creado');
+        })
         app.listen(config.port, function(){
-          user.findOrCreate({
-            name: 'Administrador',
-            username: 'Administrador',
-            password: SHA256(config.password).toString(),
-            avatar: gravatar.url(config.email, {s: '100', r: 'x', d: 'retro'}, true),
-            email: config.email,
-            pin: config.pin,
-            role: 'ADMIN'}, (err, result) => {
-            console.log('Se ha creado el usuario administrador');
-          })
           console.log(`Servidor iniciado en el puerto ${config.port}`);
         });
     }
