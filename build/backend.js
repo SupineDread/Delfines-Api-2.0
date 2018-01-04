cfef1ff5578f527e6c2140b0158244e10b19338d
SupineDread <hazielfe@gmail.com>
2018-01-04 15:16:52 -0600

    port

--- build/backend.js
+++ build/backend.js
@@ -310,10 +310,10 @@ module.exports = mongoose.model('Cliente', ClienteSchema);
 /***/ (function(module, exports) {
 
 module.exports={
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 7770,
   db: process.env.MONGODB || 'mongodb://admin:cacr2205@ds036577.mlab.com:36577/congelados-delfines',
   //db: process.env.MONGODB || 'mongodb://localhost:27017/delfines',
-  
+
 }
 
 
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 7770,
   db: process.env.MONGODB || 'mongodb://admin:cacr2205@ds036577.mlab.com:36577/congelados-delfines',
   //db: process.env.MONGODB || 'mongodb://localhost:27017/delfines',
-  
+
 }
