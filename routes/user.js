6519e1b09dac61a231d0d0a0f933599e0f99af8a
SupineDread <hazielfe@gmail.com>
2018-01-10 00:13:54 -0600

    Admin auth

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,14 @@ const express = require('express');
 const api = express.Router();
 const userController = require('../controllers/user');
 const auth = require('../middlewares/auth')
+const admin = require('../middlewares/admin')
 
 //Estas rutas solo las ve el administrador
-api.get('/users', userController.getUsers);
-api.get('/user/:id', userController.getUser);
-api.post('/user', userController.saveUser);
-api.delete('/user/:id', userController.deleteUser);
-api.put('/user/:id', userController.updateUser);
+api.get('/users', [auth.ensureAuth, admin .isAdmin], userController.getUsers);
+api.get('/user/:id', [auth.ensureAuth, admin .isAdmin], userController.getUser);
+api.post('/user',[auth.ensureAuth, admin.isAdmin], userController.saveUser);
+api.delete('/user/:id',[auth.ensureAuth, admin.isAdmin], userController.deleteUser);
+api.put('/user/:id', auth.ensureAuth, userController.updateUser);
 
 api.post('/login', userController.login)
 
